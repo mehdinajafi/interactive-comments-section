499355cfb537985e270e0e57c1b5eb21231b454a
Mehdi Najafi <dev.mehdinajafi@gmail.com>
2022-03-17 12:58:44 +0330

    Config stitches

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,45 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import { globalCss } from "stitches-config";
+
+const globalStyles = globalCss({
+  "@font-face": [
+    {
+      fontFamily: "Rubik",
+      src: "url(/fonts/Rubik-Regular.ttf) format('truetype')",
+      fontWeight: 400,
+      fontStyle: "normal",
+    },
+    {
+      fontFamily: "Rubik",
+      src: "url(/fonts/Rubik-Medium.ttf) format('truetype')",
+      fontWeight: 500,
+      fontStyle: "normal",
+    },
+    {
+      fontFamily: "Rubik",
+      src: "url(/fonts/Rubik-Bold.ttf) format('truetype')",
+      fontWeight: 700,
+      fontStyle: "normal",
+    },
+  ],
+  "*": {
+    boxSizing: "border-box",
+  },
+  "html, body": {
+    padding: 0,
+    margin: 0,
+    fontFamily:
+      "Rubik, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif",
+  },
+  a: {
+    color: "inherit",
+    textDecoration: "none",
+  },
+});
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  globalStyles();
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
--- stitches.config.js
+++ stitches.config.js
@@ -11,8 +11,31 @@ export const {
   config,
 } = createStitches({
   theme: {
-    colors: {},
+    colors: {
+      primary: "hsl(238, 40%, 52%)",
+      "primary-lt": "hsl(239, 57%, 85%)",
+      danger: "hsl(358, 79%, 66%)",
+      "danger-lt": "hsl(357, 100%, 86%)",
+      "ntrl-dk": "hsl(212, 24%, 26%)",
+      ntrl: "hsl(211, 10%, 45%)",
+      "ntrl-lt": "hsl(223, 19%, 93%)",
+      "ntrl-ltr": "hsl(228, 33%, 97%)",
+      "ntrl-min": "hsl(0, 0%, 100%)",
+    },
+    fonts: {
+      base: "Rubik, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif",
+    },
+    fontSizes: {
+      base: "1rem",
+    },
+    fontWeights: {
+      regular: 400,
+      medium: 500,
+      bold: 700,
+    },
   },
-  media: {},
+  media: {
+    sm: "(max-width: 375px)",
+  },
   utils: {},
 });
